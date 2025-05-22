ec192d3a4518d0300178c89c4f65399e9c9df478
gakusugino <g-sugino@macro-eg.co.jp>
2025-05-22 16:26:38 +0900

    redux dev toolsプラグインをインストール

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { createStore } from 'redux';
 import allReducer from './reducers';
 
-const store = createStore(allReducer)
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
