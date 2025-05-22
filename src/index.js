e362e33cffd11480599a54676e830fa8d2c14362
gakusugino <g-sugino@macro-eg.co.jp>
2025-05-22 17:14:48 +0900

    Reduxストアをアプリに接続

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import allReducer from './reducers';
+import { Provider } from 'react-redux';
+
 
 const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
