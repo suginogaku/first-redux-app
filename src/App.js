edcdb63901774ee6cd4f687ae99f313a59307da7
gakusugino <g-sugino@macro-eg.co.jp>
2025-05-23 15:59:12 +0900

    useSelector使ってみた

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+
   return (
     <div className="App">
-      <div>Hello redux</div>
+      <h1>Hello redux</h1>
+      <h3>Counter: {counter}</h3>
+      {isLogin ? (
+        <h3>ログイン中</h3>
+      ) : (
+        <h3>ログインしてください</h3>
+      )}
     </div>
   );
 }
